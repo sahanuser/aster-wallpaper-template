f8fad17920f4f2b7d33ef0deddc1acf674d781da
sahanuser <sahan.user@gmail.com>
2023-01-01 11:11:13 +0530

    first commit

---
+++ downloading.py
@@ -0,0 +1,18 @@
+import subprocess
+import wget
+import requests
+import ast
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+import shutil
+import os
+
+url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+r = requests.get(url)
+data = ast.literal_eval(r.content.decode('utf-8'))
+print(data["url"])
+name = wget.download(data["url"])
+print (name)
+destination = f"usr/share/backgrounds/asterlinux/{name}"
+shutil.move(name,destination)
