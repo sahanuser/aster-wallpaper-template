a802c31b9306d17f57270e2a81013ba22b0806a1
AsterLinux <92189964+asterlinux@users.noreply.github.com>
2023-01-02 14:55:06 +0530

    Update downloadimg.py

--- downloadimg.py
+++ downloadimg.py
@@ -11,10 +11,10 @@ import os
 url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 r = requests.get(url,params = {"hd": "True"})
 
-title = r["title"].replace(" ","_").replace(":","_") + ".jpg"
+title = r["title"]
 
 data = ast.literal_eval(r.content.decode('utf-8'))
 print(data["url"])
 wget.download(data["url"])
-destination = f"aster-wallpaper/usr/share/backgrounds/asterlinux/{title}"
+destination = f"aster-wallpaper/usr/share/backgrounds/asterlinux/{title}.jpg"
 shutil.move(name,destination)
